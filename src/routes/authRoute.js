f1b5f5586dd5ac24e83481a58226e27c1a7b450e
PELUMIJOY <faslusky@gmail.com>
2025-06-24 16:08:22 +0100

    remove session

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -10,25 +10,7 @@ router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
 router.get("/protected", verifyToken, getProtectedData);
-// router.get(
-//   "/google",
-//   passport.authenticate("google", { scope: ["profile", "email"] })
-// );
-// router.get(
-//   "/google/callback",
-//   passport.authenticate("google", { session: false }),
-//   oauthCallback
-// );
 
-// router.get(
-//   "/facebook",
-//   passport.authenticate("facebook", { scope: ["email"] })
-// );
-// router.get(
-//   "/facebook/callback",
-//   passport.authenticate("facebook", { session: false }),
-//   oauthCallback
-// );
 router.get(
   "/google",
   (req, res, next) => {
@@ -43,7 +25,7 @@ router.get(
 
 router.get(
   "/google/callback",
-  passport.authenticate("google", { session: false }),
+  passport.authenticate("google", ),
   oauthCallback
 );
 
